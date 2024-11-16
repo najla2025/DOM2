1e406922abf3d6149201444d1fc3727e0cbfeae6
Mac <najlahammoudi26@gmail.com>
2024-11-16 18:33:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  // Fonction pour changer la couleur de la boîte
+  function changeColor() {
+    const colorBox = document.getElementById('color-box'); 
+    colorBox.style.backgroundColor = getRandomColor(); 
+  }
+  
+  // Sélectionner le bouton "Change Color" et ajouter un écouteur d'événements
+  document.getElementById('change-color-btn').addEventListener('click', changeColor);
+  
+
+ 
